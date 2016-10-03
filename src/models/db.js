fe8601713f8fbc8c568edfab4f4cfdf1137dfc56
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-10-03 00:53:06 -0500

    minus commas

--- src/models/db.js
+++ src/models/db.js
@@ -1,16 +1,16 @@
 var mongoose = require('mongoose');
 
-var connectionString = process.env.DB_HOST;
-mongoose.connect(connectionString);
+var connectionString = process.env.DB_HOST
+mongoose.connect(connectionString)
 
 mongoose.connection.on('connected', function() {
-  console.log('mongoose connected to ' + connectionString);
+  console.log('mongoose connected to ' + connectionString)
 })
 
 mongoose.connection.on('error', function(err) {
-  console.log('mongoose connection error ' + err);
+  console.log('mongoose connection error ' + err)
 })
 
 mongoose.connection.on('disconnected', function() {
-  console.log('mongoose disconnected');
+  console.log('mongoose disconnected')
 })
